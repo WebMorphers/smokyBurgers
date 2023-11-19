aa6034fef56af90486737c9137eeac6c42e49cb9
othsoh <sohabsouri@gmail.com>
2023-11-19 19:15:30 +0100

    t

---
+++ src/components/More-than-burger/More-than-burger.tsx
@@ -0,0 +1,12 @@
+
+const Moreburger = () => {
+  return (
+    <div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Moreburger
---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,12 @@
+
+const navbar = () => {
+  return (
+    <div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default navbar
