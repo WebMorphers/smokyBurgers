8334d0878fb80a7ec5645f2cb069ac9b8384b3ab
othsoh <sohabsouri@gmail.com>
2023-11-20 17:46:17 +0100

    t

--- src/components/More-than-burger/More-than-burger.tsx
+++ src/components/More-than-burger/More-than-burger.tsx
@@ -1,20 +1,21 @@
-
+import moreburger from '../../assets/more-burger.png'
+import './More-than-burger.css'
 const Moreburger = () => {
   return (
-    <div className="container">
-        <div className="flex ">
-            <div>
-
+    <div className=" flex items-center justify-center">
+        <div className=" grid md:grid-cols-3 ">
+            <div className='bg-[#FB8A8A] rounded-[40px] md:m-28'>
+              <img className='pt-24' src={moreburger} />
             </div>
-            <div>
-              <div>
-                <h1>More than just a burger</h1>
+            <div className='col-span-2'>
+              <div className='text-3xl text-[#FB8A8A] font-semibold'>
+                <h1 className='title'>More than just a burger</h1>
               </div>
               <div>
                 Giving the customer a burger that ticks all the boxes from taste to price to quality. 
               </div>
             </div>
-        </div>
+      </div>
     </div>
   )
 }
