8094da7872c3594196041a51be112b98ab340a0a
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-11-20 20:11:30 +0000

    d

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,32 @@
+import Navbar from "../Navbar/navbar";
+import img from "../../assets/imaagehero.png";
+
+import "./hero.css";
+import { motion } from "framer-motion";
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  const animationVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
 
-export default Hero
+  const heroStyle = {
+    backgroundImage: `url(${img})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center center",
+    height: "100vh",
+    width: "100%",
+  };
+
+  return (
+    <div
+      className="hero w-full relative z-0 flex flex-col justify-center items-center bg-[#160808]"
+      style={heroStyle}
+      id="hero"
+    >
+      <Navbar />
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,12 +1,134 @@
+import logo from "../../assets/logohd.png";
+import { useEffect, useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
-const navbar = () => {
+import "./navbar.css";
+
+const Navbar = () => {
+  const [nav, setnav] = useState(false);
+  function chang() {
+    setnav(!nav);
+  }
+  const scrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      window.scrollTo({
+        top: section.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  };
+  useEffect(() => {
+    const internalLinks = document.querySelectorAll('a[href^="#"]');
+    internalLinks.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute("href")?.slice(1);
+        if (targetId) {
+          scrollToSection(targetId);
+        }
+      });
+    });
+  }, []);
+
   return (
-    <div>
-        <div>
-            
-        </div>
-    </div>
-  )
-}
+    <div
+      id="nav"
+      className="flex  w-full absolute top-0  justify-between items-center h-24 mx-auto px-10 z-10 text-white"
+    >
+      <a href="/">
+        <img className="h-20 w-23" src={logo} />
+      </a>
+      <ul className="hidden md:flex">
+        <li className="p-4">
+          <a
+            className="no-underline mx-4 cursor-pointer font-normal	text-white hover:text-amber-400"
+            href="/"
+          >
+            About us
+          </a>
+        </li>
+        <li className="p-4">
+          <a
+            className="no-underline mx-4 cursor-pointer font-normal	text-white hover:text-amber-400"
+            onClick={() => scrollToSection("projects")}
+            href="#about"
+          >
+            Menu
+          </a>
+        </li>
+        <li className="p-4">
+          <a className="no-underline mx-4 cursor-pointer font-normal	text-white hover:text-amber-400">
+            Favorites
+          </a>
+        </li>
+        <li className="p-4">
+          <a className="no-underline mx-4 cursor-pointer font-normal	text-white hover:text-amber-400">
+            Find us
+          </a>
+        </li>
+      </ul>
+      <div></div>
 
-export default navbar
+      <div onClick={chang} className="block md:hidden">
+        {nav ? (
+          <AiOutlineClose size={20} color={"white"} />
+        ) : (
+          <AiOutlineMenu size={20} color={"white"} />
+        )}
+      </div>
+      <div
+        className={
+          nav
+            ? "fixed left-0 top-0  h-full w-full border-r border-r-gray-900  bg-[#FFD596] ease-in-out duration-500 flex items-center justify-center flex-col"
+            : "ease-in-out duration-500 fixed left-[-100%]"
+        }
+      >
+        <div onClick={chang} className="absolute top-20 right-20 z-10">
+          <AiOutlineClose size={20} color={"white"} />
+        </div>
+
+        <ul className="flex flex-col justify-center items-center">
+          <a href="/" onClick={() => scrollToSection("hello")}>
+            <img className="h-20 w-23" src={logo} alt="" />
+          </a>
+          <div className="absolute top-0 object-top z-0"></div>
+          <li className="p-4 ">
+            <a
+              className="no-underline mx-4 cursor-pointer font-thin text-black hover:text-amber-400"
+              onClick={() => {
+                scrollToSection("projects"), chang();
+              }}
+              href="/"
+            >
+              ACCUEIL
+            </a>
+          </li>
+          <li className="p-4">
+            {" "}
+            <a
+              className="no-underline mx-4 cursor-pointer font-thin text-black hover:text-amber-400"
+              onClick={() => {
+                scrollToSection("projects"), chang();
+              }}
+              href="#about"
+            >
+              A PROPOS
+            </a>
+          </li>
+          <li className="p-4">
+            <a
+              className="no-underline mx-4 cursor-pointer font-thin text-black hover:text-amber-400"
+              onClick={() => scrollToSection("projects")}
+              href="/menu"
+            >
+              MENU
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
