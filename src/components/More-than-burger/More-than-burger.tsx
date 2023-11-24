222d8858cbde3f57562bf3825f072eb6eab8f82d
othsoh <sohabsouri@gmail.com>
2023-11-24 14:57:14 +0100

    t

--- src/components/More-than-burger/More-than-burger.tsx
+++ src/components/More-than-burger/More-than-burger.tsx
@@ -4,13 +4,13 @@ import burger from "../../assets/burger.svg"
 const Moreburger = () => {
   return (
     <div className="cont">
-        <div className=" flex max-sm:flex-col items-center justify-center gap-12 relative">
+        <div className=" flex max-sm:flex-col items-center justify-between gap-12 relative">
             <div className='bg-[#FB8A8A] rounded-[40px] flex justify-center'>
               <img className='pt-24 ' src={moreburger}  />
             </div>
             <div className='col-span-2 flex items-center flex-col justify-center gap-12 relative '>
               <div className='text-3xl sm:text-5xl text-[#FB8A8A] font-semibold'>
-                <h1 className='title text-[#FB8A8A]'>More than just a burger</h1>
+                <h1 id='light' className='title text-[#FB8A8A]'>More than just a burger</h1>
               </div>
               <div className='description text-xl text-gray-700'>
                 Giving the customer a burger that ticks all the boxes from taste to price to quality. 
