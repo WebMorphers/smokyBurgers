91b0a536686205caf38cbb546700275142fe4513
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-11-25 22:54:28 +0000

    1

--- src/components/About-us/About-us.tsx
+++ src/components/About-us/About-us.tsx
@@ -16,21 +16,23 @@ import vid12 from '../../assets/vid-toto.mp4';
 
 const AboutUs = () => {
   return (
-    <div className="py-8 px-20 max-sm:px-12 xl:px-36 flex flex-col justify-center items-center gap-5">
-      <h1 className="text-6xl title text-center">Taste Like Never Before .</h1>
+    <div className="py-8 px-4 sm:px-8 md:px-12 lg:px-20 xl:px-36 flex flex-col justify-center items-center gap-5">
+    <h1 className="text-4xl sm:text-5xl lg:text-6xl title text-center">Taste Like Never Before .</h1>
 
-      <h1 className="text-sky-950 text-center">
-        Suivez{" "}
-        <a
-          href="https://www.instagram.com/comptoir.sucre/"
-          target="-blank"
-          className="visited:text-black  hover:text-neutral-700 text-inherit"
-        >
-          <span className="underline">@comptoir.sucre</span>
-        </a>{" "}
-        sur Instagram pour plus d'inspiration en brownies gourmets !
-      </h1>
-      <div className=' grid grid-cols-4 grid-rows-3 gap-9'>
+    <h1 className="text-sky-950 text-center">
+      Suivez{" "}
+      <a
+        href="https://www.instagram.com/comptoir.sucre/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="visited:text-black hover:text-neutral-700 text-inherit"
+      >
+        <span className="underline">@comptoir.sucre</span>
+      </a>{" "}
+      sur Instagram pour plus d'inspiration en brownies gourmets !
+    </h1>
+
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
       <div><HoverVideoPlayer videoSrc= {vid1} /></div> 
       <div><HoverVideoPlayer videoSrc= {vid2} /></div> 
       <div><HoverVideoPlayer videoSrc= {vid3} /></div> 
