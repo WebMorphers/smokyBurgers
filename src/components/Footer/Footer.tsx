53c5eb5eee610f6e25e4c8e03ccca6263c16f46d
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-11-25 23:44:07 +0000

    1

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,24 +10,20 @@ const Footer = () => {
   return (
     <div>
     <div className="bg-black flex flex-col justify-center items-center p-10">
-      <ul className="flex flex-row justify-center items-center gap-12 text-white  ">
+      <ul className="grid lg:grid-cols-6  grid-cols-2  max-lg:gap-8  text-white  ">
       <a href=""> <li className="footer font-normal">Menu</li></a>
       <a href=""><li className="footer font-normal">About us</li></a>
       <a href=""> <li className="footer font-normal">Order Now</li></a>
       <a href=""><li className="footer font-normal">Location</li></a>
        <a href=""> <li className="footer font-normal">Contact Us</li></a>
-        <li className="flex justify-end items-end gap-3">
+        <li className="flex justify-end max-lg:justify-center max-lg:items-center items-end  gap-3">
         <img src={insta}  alt="" />          
-          <img src={glovo} width={25} alt="" />          
-          <img src={tiktok}  alt="" />    
-
-          <img className="h-20 w-23" src={logo} />
-
-        </li>
-        
+        <img src={glovo} width={25} alt="" />          
+        <img src={tiktok}  alt="" />     
+         <img className="h-20 w-23" src={logo} /> 
+        </li> 
       </ul>
-      <hr className="border-t-2 border-white w-[60%] m-3" />
-
+      <hr className="border-t-2 border-white w-[60%] m-3" /> 
       <div className="flex flex-row justify-center items-center gap-12 text-white  ">
        <p>©2023 <a href="" className="text-amber-600 hover:text-orange-500">WEBMORPHERS</a> . All rights reserved.</p>
     </div>
