ab94ca495761f288fd31abab4a4ae31aa5d21c5e
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-11-26 00:21:38 +0000

    f

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,29 +4,59 @@ import insta from "../../assets/instagram-svg.svg";
 import glovo from "../../assets/glovo-svg.svg";
 import tiktok from "../../assets/tiktok-svg.svg";
 
-
 const Footer = () => {
   return (
     <div>
-    <div className="bg-black flex flex-col justify-center items-center p-10">
-      <ul className="grid lg:grid-cols-6  grid-cols-2  max-lg:gap-8  text-white  ">
-      <a href=""> <li className="footer font-normal">Menu</li></a>
-      <a href=""><li className="footer font-normal">About us</li></a>
-      <a href=""> <li className="footer font-normal">Order Now</li></a>
-      <a href=""><li className="footer font-normal">Location</li></a>
-       <a href=""> <li className="footer font-normal">Contact Us</li></a>
-        <li className="flex justify-end max-lg:justify-center max-lg:items-center items-end  gap-3">
-        <img src={insta}  alt="" />          
-        <img src={glovo} width={25} alt="" />          
-        <img src={tiktok}  alt="" />     
-         <img className="h-20 w-23" src={logo} /> 
-        </li> 
-      </ul>
-      <hr className="border-t-2 border-white w-[60%] m-3" /> 
-      <div className="flex flex-row justify-center items-center gap-12 text-white  ">
-       <p>©2023 <a href="" className="text-amber-600 hover:text-orange-500">WEBMORPHERS</a> . All rights reserved.</p>
-    </div>
-    </div>
+      <div className="bg-black flex flex-col justify-center items-center p-10">
+        <ul className="grid lg:grid-cols-6  grid-cols-2  max-lg:gap-8  text-white  ">
+          <a href="" className="hover:text-red-600">
+            {" "}
+            <li className="footer font-normal">Menu</li>
+          </a>
+          <a href="" className="hover:text-red-600">
+            <li className="footer font-normal">About us</li>
+          </a>
+          <a href="" className="hover:text-red-600">
+            {" "}
+            <li className="footer font-normal">Order Now</li>
+          </a>
+          <a href="" className="hover:text-red-600">
+            <li className="footer font-normal">Location</li>
+          </a>
+          <a href="" className="hover:text-red-600">
+            {" "}
+            <li className="footer font-normal">Contact Us</li>
+          </a>
+          <li className="flex justify-end max-lg:justify-center max-lg:items-center items-end  gap-3">
+            <a
+              href="https://www.instagram.com/smokyburgersmaroc/"
+              target="-blank"
+            >
+              <img className="hover:scale-125" src={insta} alt="" />{" "}
+            </a>
+            <a
+              href="https://glovoapp.com/ma/fr/casablanca/smoky-burgers/?utm_campaign=maplinks&utm_medium=organic&utm_source=googlemaps"
+              target="-blank"
+            >
+              <img className="hover:scale-125" src={glovo} width={25} alt="" />{" "}
+            </a>
+            <a href="https://www.tiktok.com/@smokyburgers" target="-blank">
+              <img className="hover:scale-125" src={tiktok} alt="" />{" "}
+            </a>
+            <img className="h-20 w-23" src={logo} />
+          </li>
+        </ul>
+        <hr className="border-t-2 border-white w-[60%] m-3" />
+        <div className="flex flex-row justify-center items-center gap-12 text-white  ">
+          <p>
+            ©2023{" "}
+            <a href="" className="text-amber-600 hover:text-orange-500">
+              WEBMORPHERS
+            </a>{" "}
+            . All rights reserved.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/More-than-burger/More-than-burger.tsx
+++ src/components/More-than-burger/More-than-burger.tsx
@@ -1,30 +1,39 @@
-import moreburger from '../../assets/more-burger.png'
-import './More-than-burger.css'
-import burger from "../../assets/burger.svg"
+import moreburger from "../../assets/more-burger.png";
+import "./More-than-burger.css";
+import burger from "../../assets/burger.svg";
 const Moreburger = () => {
   return (
     <div className="cont">
-        <div className=" flex max-sm:flex-col items-center justify-between gap-12 relative">
-            <div className='bg-[#FB8A8A] rounded-[40px] flex justify-center'>
-              <img className='pt-24 ' src={moreburger}  />
-            </div>
-            <div className='col-span-2 flex items-center flex-col justify-center gap-12 relative '>
-              <div className='text-3xl sm:text-5xl text-[#FB8A8A] font-semibold'>
-                <h1 id='light' className='title text-[#FB8A8A]'>More than just a burger</h1>
-              </div>
-              <div className='description text-xl text-gray-700'>
-                Giving the customer a burger that ticks all the boxes from taste to price to quality. 
-              </div>
-              
-              <img src={burger} className=' max-md:w-16 absolute bottom-0 top-0 my-auto right-0 animate-bounceRight' alt="" />
+      <div className=" flex max-sm:flex-col items-center justify-between xl:justify-center xl:gap-40  gap-12 relative">
+        <div className="bg-[#FB8A8A] rounded-[40px] flex justify-center">
+          <img className="pt-24 " src={moreburger} />
+        </div>
+        <div className="col-span-2 flex items-center flex-col justify-center gap-12 relative ">
+          <div className="text-3xl sm:text-5xl text-[#FB8A8A] font-semibold">
+            <h1 id="light" className="title text-[#FB8A8A]">
+              More than just a burger
+            </h1>
+          </div>
+          <div className="description text-xl text-gray-700">
+            Giving the customer a burger that ticks all the boxes from taste to
+            price to quality.
+          </div>
 
-            </div>
+          <img
+            src={burger}
+            className=" max-md:w-16 absolute bottom-0 top-0 my-auto right-0 animate-bounceRight"
+            alt=""
+          />
+        </div>
 
-            <img src={burger} className=' max-md:w-16 absolute top-0 -left-10 max-sm:left-0 animate-bounceLeft' alt="" />
-
+        <img
+          src={burger}
+          className=" max-md:w-16 absolute top-0 -left-10 max-sm:left-0 animate-bounceLeft"
+          alt=""
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Moreburger
+export default Moreburger;
