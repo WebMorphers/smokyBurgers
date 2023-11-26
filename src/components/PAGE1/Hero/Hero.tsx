6da598c2381627ed67b82605bb2041450a0ebc6f
moncef <145851559+WebMorphers@users.noreply.github.com>
2023-11-26 18:36:07 +0000

    s

--- src/components/PAGE1/Hero/Hero.tsx
+++ src/components/PAGE1/Hero/Hero.tsx
@@ -2,6 +2,7 @@ import Navbar from "../Navbar/navbar";
 import img from "../../../assets/imaagehero.png";
 
 import "./hero.css";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   const heroStyle = {
@@ -22,9 +23,12 @@ const Hero = () => {
       <h1 className=" title__hero text-9xl max-sm:text-8xl text-center">
         SMOKY BURGERS
       </h1>
-      <button className="bg-[#FE181A] text-white om text-xl p-2 px-3 border-gray-400 border rounded-2xl bottom-36 ">
-        Our menu
-      </button>
+      <Link to="/menu">
+        {" "}
+        <button className="bg-[#FE181A] text-white om text-xl p-2 px-3 border-gray-400 border rounded-2xl bottom-36 transform transition-transform duration-500 ease-in-out hover:scale-110 ">
+          MENU
+        </button>
+      </Link>
     </div>
   );
 };
